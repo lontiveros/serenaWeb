5f5a45a2d0e4901d8e1d9743edac25e6e46a7378
Luis Ontiveros <leomkr@gmail.com>
2020-06-08 00:53:39 -0300

    Se ajusta el nombre del proyecto.

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/dartWeb'));
+app.use(express.static('./dist/serena-web'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/dartWeb/'}),
+    res.sendFile('index.html', {root: 'dist/serena-web/'}),
 );
 
 app.listen(process.env.PORT || 8080);
